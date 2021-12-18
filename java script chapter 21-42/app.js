976b04b3c994c09a1ae3faf319b0f5c48d4f50d9
AbdulWasayKhann <91083782+AbdulWasayKhann@users.noreply.github.com>
2021-12-18 15:18:36 +0500

    assignment

---
+++ java script chapter 21-42/app.js
@@ -0,0 +1,108 @@
+// Changing case-----------
+// UPPER CASE___________
+// var cities = ["Karachi","Lahor","Islamabad","Quttea"];
+// var UppercaseText = cities.tolowercase
+// cities = cities.toUppercase();
+// alert(cities);
+
+//  LOWER CASE___________
+// var Newcities = ["Karachi","Lahor","Islamabad","Quttea"];
+// // var UppercaseText = cities.tolowercase
+// Newcities = Newcities.toLowercase();
+// alert(newcities);
+
+
+// Strings : Measuring lengthand extracting parts--------------
+// SLICE______________
+// var OldAnimals = ["Cow","Parrot","Dog","Cat","Fish"];
+// var NewAnimal = OldAnimals.slice(1,4);
+// alert(NewAnimal);
+
+// // LENGTH________________
+// var Month = prompt("Enter your birth Month");
+// var LengthOfMonth = Month.length;
+// alert (LengthOfMonth);
+
+
+// Strings:Finding segments----------
+// INDEX OF________________
+// var text = ["A","B","C","D","E"];
+// var newtext = text.indexOf("C"); 
+// alert(newtext);
+
+// // LAST INDEXOF_____________
+// var Alpha = ["Who are you are you Ghost"];
+// var newtAlpha = Alpha.lastIndexOf("are"); 
+// alert(newtext);
+
+
+// Strings:Replacing characters--------------
+// REPLACE_____________
+// var pera = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, autem amet impedit sunt alias nulla.";
+// var customPera = pera.replace("sit amet consectetur adipisicing elit. Quia");
+// alert(customPera);
+
+
+// Rounding numbers------------------
+// var num = 3.9 
+// num = Math.round(num);
+// alert(num);
+// CEIL___________
+// var scoreAvg = Math.ceil(.000001);
+// alert(scoreAvg);
+// // FLOOR________________
+// var NewscoreAvg = Math.floor(.999999);
+// alert(NewscoreAvg);
+
+
+// Generating random numbers
+// var MyNumber = Math.random();
+// alert(MyNumber);
+
+
+// Converting strings to integers and decimals
+// var age = 25;
+// var Newage = age -15;
+// alert(Newage);
+
+// var HouseNo = "45" + "50";
+// alert(HouseNo);
+
+// var Letter = "Hello";
+// var num1 = 300;
+// var Result = "Letter" - 300;
+// alert(Result);
+
+
+// Converting strings to numbers, numbers to strings
+// var numberString = "50"
+// var num = Number(numberString);
+// alert(num);
+
+// var numberToNumber = 5678;
+// var numberToString = numberAsNumber.toString();
+// alert(numberToString);
+
+
+// Controlling the length of decimals
+// var decimal = 1.2345456
+// var customDecimal = decimal.toFixed(3);
+// alert(customDecimal);
+
+
+// Getting the current date and time
+// var currentDate = new Date();
+// alert(currentDate);
+
+
+// Extracting parts of the date and time
+// var d = new Date();
+// var currentMonth = d.getMonth();
+// var dayOfMonth = d.getDate();
+// var currYr = d.getFullYear();
+// var currentHrs = d.getHours();
+// var currMins = d.getMinutes();
+// var currSecs = d.getSeconds()
+// var currMills = d.getMilliseconds();
+// var millsSince = d.getTime();
+// alert(d);
